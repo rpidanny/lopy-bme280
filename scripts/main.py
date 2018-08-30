da040bbb0948b11c1b59dc870e11fc8fe3b39d10
rpidanny <abhishekmaharjan1993@gmail.com>
2018-08-30 19:51:52 +0200

    Print bme280 sensor reading to console

--- scripts/main.py
+++ scripts/main.py
@@ -1,1 +1,11 @@
 # main.py -- put your code here!
+import time
+from machine import I2C
+from bme280 import BME280
+
+i2c = I2C()
+bme = BME280(i2c=i2c)
+
+while True:
+  print("Temp: "+ bme.temperature + ", Pressure: " + bme.pressure +", Humidity: " + bme.humidity)
+  time.sleep(1)

85baf7d95778ef8f33c827fd1bbb08f4482c205b
rpidanny <abhishekmaharjan1993@gmail.com>
2018-08-30 19:59:00 +0200

    Change BME280 sensor mode to 5

--- scripts/main.py
+++ scripts/main.py
@@ -1,10 +1,10 @@
 # main.py -- put your code here!
 import time
 from machine import I2C
-from bme280 import BME280
+from bme280 import BME280, BME280_OSAMPLE_16
 
 i2c = I2C()
-bme = BME280(i2c=i2c)
+bme = BME280(i2c=i2c, mode=BME280_OSAMPLE_16)
 
 while True:
   print("Temp: "+ bme.temperature + ", Pressure: " + bme.pressure +", Humidity: " + bme.humidity)

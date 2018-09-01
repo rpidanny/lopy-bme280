88a11d06145179e5b179868ceb288c5677b9f61b
rpidanny <abhishekmaharjan1993@gmail.com>
2018-09-01 21:13:06 +0200

    Add wifi check and connect on main loop

--- scripts/main.py
+++ scripts/main.py
@@ -1,17 +1,19 @@
 # main.py -- put your code here!
 import time
 from machine import I2C
+from network import WLAN
+
 from mqtt import MQTTClient
 from bme280 import BME280, BME280_OSAMPLE_16
-from config import adafruit
 
-
-def sub_cb(topic, msg):
-    print(msg)
+from config import adafruit, known_nets
+from util.wifi import connect_wifi
 
 
 i2c = I2C()
 bme = BME280(i2c=i2c, mode=BME280_OSAMPLE_16)
+
+wl = WLAN()
 
 client = MQTTClient(
     "lopy-nl",
@@ -20,13 +22,14 @@ client = MQTTClient(
     password=adafruit['apikey'],
     port=1883)
 
-client.set_callback(sub_cb)
 client.connect()
-# client.subscribe(topic="rpidanny/feeds/lopy-nl")
 
 while True:
     print("Temp: " + bme.temperature + ", Pressure: " + bme.pressure +
           ", Humidity: " + bme.humidity)
+    if not wl.isconnected():
+        connect_wifi(known_nets)
+        client.connect()
     client.publish(topic=adafruit['user'] + "/feeds/bme280_temp", msg=bme.temperature)
     client.publish(topic=adafruit['user'] + "/feeds/bme280_pressure", msg=bme.pressure)
     client.publish(topic=adafruit['user'] +  "/feeds/bme280_humidity", msg=bme.humidity)
